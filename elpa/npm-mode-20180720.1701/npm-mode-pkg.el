d2ab724d9b70f8690ac4ef5393770ece57b2eef0
jonroby <joncaroby@gmail.com>
2019-03-24 10:59:07 -0700

    init

---
+++ elpa/npm-mode-20180720.1701/npm-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "npm-mode" "20180720.1701" "minor mode for working with npm projects" '((emacs "24.1")) :commit "4f4b9fc2c07290ae87f65179df95be5221e76bf2" :keywords '("convenience" "project" "javascript" "node" "npm") :authors '(("Allen Gooch" . "allen.gooch@gmail.com")) :maintainer '("Allen Gooch" . "allen.gooch@gmail.com") :url "https://github.com/mojochao/npm-mode")
